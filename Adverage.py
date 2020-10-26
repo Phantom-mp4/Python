6c86895757895b6a53cdd7a85d159bc29baeea22
Phantom-mp4 <73188105+Phantom-mp4@users.noreply.github.com>
2020-10-26 11:45:31 -0500

    Delete Adverage.py

--- Adverage.py
+++
@@ -1,8 +0,0 @@
-Number1 = input("Give me an integer."),
-Number2 = input("Give me another integer."),
-Number3 = input("Give me another integer."),
-Number4 = input("Give me another integer."),
-
-Average = (Number1+Number2+Number3+Number4/4)
-
-print(Average) 
